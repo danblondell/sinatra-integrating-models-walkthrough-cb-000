43211852e37a45257238513da45811849d06aa2f
Dan Blondell <danblondell@gmail.com>
2017-11-19 02:14:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    @analized_text = TextAnalyzer.new(params[:user_text])
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
 
     erb :results
   end
